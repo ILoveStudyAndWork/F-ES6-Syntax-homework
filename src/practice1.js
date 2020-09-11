898de0112ac8b904dc810e0ad580c4b88b60d435
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-11 17:02:02 +0800

    [fengyi_ling] feat:comlete practice1 and practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
+const parseData =({data,column}) => {
+    let output = [];
+    data.map(record => {
+        let item = {};
+        column.map((col,index)=> {
+            item[col.name] = record[index];
+        })
+        output.push(item);
+    })
+    return output;
+}
 export { parseData };
